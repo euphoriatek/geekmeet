8a60f6ff9355a31396bcec2477f3832db9afeb91
vipin <vipin@entrusters.com>
2016-12-12 18:05:33 +0530

    Added api for submit event

--- src/app/model/api-method.service.ts
+++ src/app/model/api-method.service.ts
@@ -412,7 +412,19 @@ export class ApiMethodService {
 		}, (error) => failure(error));
 	} 
 
+   //add event
 
+	addEvent(userData,callBack, failure){
+		let headers = new Headers({ 'Auth': "Bearer "+ localStorage.getItem('auth_token')});
+		let options = new RequestOptions({ headers: headers });
+		this.http.post('http://2016.geekmeet.com/admin/v1/add_event',userData,options).map(res=>res.json())
+		.subscribe((res)=>{
+			if(callBack)
+			{
+				callBack(res);
+			}
+		}, (error) => failure(error));
+	}
 
 
 
